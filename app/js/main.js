887017acc87e36307620a108722c399ee2f8ee4d
Vadim Losenkov <vprog0729@gmail.com>
2021-09-29 13:53:02 +0300

    added articles

--- app/js/main.js
+++ app/js/main.js
@@ -6,20 +6,70 @@ $(function() {
     $(document.body).toggleClass('open')
   })
 })
+/*
+ymaps.ready(init);
 
-function initMap() {
-  var coordinates = {lat: 47.212325, lng: 38.933663},
+function init () {
+    var myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 100
+        }),
 
-      map = new google.maps.Map(document.getElementById('map'), {
-          center: coordinates
-      }),
+        // Создаем геообъект с типом геометрии "Точка".
+        myGeoObject = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.8, 37.8]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                iconContent: 'Метка',
+                balloonContent: 'Меня можно перемещать'
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'twirl#redStretchyIcon',
+            // Метку можно перемещать.
+            draggable: true
+        }),
 
-      marker = new google.maps.Marker({
-          position: coordinates,
-          map: map,
-          animation: google.maps.Animation.BOUNCE
-      });
-}
+        // Создаем метку с помощью вспомогательного класса.
+        myPlacemark1 = new ymaps.Placemark([55.8, 37.6], {
+            // Свойства.
+            // Содержимое иконки, балуна и хинта.
+            iconContent: '1',
+            balloonContent: 'Балун',
+            hintContent: 'Стандартный значок метки'
+        }, {
+            // Опции.
+            // Стандартная фиолетовая иконка.
+            preset: 'twirl#violetIcon'
+        }),
+
+        myPlacemark2 = new ymaps.Placemark([55.76, 37.56], {
+            // Свойства.
+            hintContent: 'Собственный значок метки'
+        }, {
+            // Опции.
+            // Своё изображение иконки метки.
+            iconImageHref: '/maps/doc/jsapi/2.x/examples/images/myIcon.gif',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-3, -42]
+        });
+    
+    myMap.controls.add('smallZoomControl');
+    // Добавляем все метки на карту.
+    myMap.geoObjects
+        .add(myPlacemark1)
+        .add(myPlacemark2)
+        .add(myGeoObject);
+} */
 
 new Swiper(".swiper-images", {
   slidesPerView: 1,
